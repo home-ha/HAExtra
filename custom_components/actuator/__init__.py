552bb7126b51c7ec96fc8511690d1e76d2c4c951
Yonsm <Yonsm@qq.com>
2019-07-29 13:08:32 +0800

    refine actuator service key

--- custom_components/actuator/__init__.py
+++ custom_components/actuator/__init__.py
@@ -1,11 +1,4 @@
-import json
-import requests
-import os,re,random,string
-import time
-import base64
-import hass_frontend
-from urllib import parse
-from threading import Thread,Event
+
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 
@@ -21,9 +14,9 @@ ACTUATE_SCHEMA = vol.Schema({
     vol.Required('sensor_values'): list,
     vol.Required('entity_id'): cv.string,
     vol.Optional('entity_attr'): cv.string,
-    vol.Required('service'): cv.string,
+    vol.Optional('service'): cv.string,
     vol.Optional('service_attr'): cv.string,
-    vol.Required('values'): list,
+    vol.Required('entity_values'): list,
 })
 
 _hass = None
@@ -35,7 +28,6 @@ def setup(hass, config):
     return True
 
 def actuate(call):
-    return
     sensor_id = call.data.get('sensor_id')
     sensor_attr = call.data.get('sensor_attr')
     sensor_values = call.data.get('sensor_values')
@@ -44,40 +36,39 @@ def actuate(call):
     entity_attr = call.data.get('entity_attr')
     service = call.data.get('service')
     service_attr = call.data.get('service_attr')
-    values = call.data.get('values')
+    entity_values = call.data.get('entity_values')
     domain = entity_id[:entity_id.find('.')]
 
-    _LOGGER.error("%s, sensor_id: %s, sensor_attr: %s, sensor_values: %s", call, sensor_id, sensor_attr, sensor_values)
-    _LOGGER.error("%s, entity_id: %s, entity_attr: %s, service: %s, service_attr: %s, values: %s", call.data, entity_id, entity_attr, service, service_attr, values)
-
     sensor_state = _hass.states.get(sensor_id)
-    sensor_value = float(sensor_state.state if sensor_attr is None else sensor_state.attributes.get(sensor_attr))
-    _LOGGER.error("sensor_id: %s, sensor_value: %s", sensor_id, sensor_value)
+    if sensor_state is None:
+        _LOGGER.error("No sensor state for %s", sensor_id)
+        return
 
-    state = _hass.states.get(sensor_id)
+    sensor_value = float(sensor_state.state if sensor_attr is None else sensor_state.attributes.get(sensor_attr))
+    _LOGGER.debug("%s %s= %s", sensor_id, sensor_attr or '', sensor_value)
+
+    state = _hass.states.get(entity_id)
     state_value = state.state
 
     i = len(sensor_values) - 1
     while i >= 0:
         if sensor_value >= sensor_values[i]:
-            value = values[i]
-            entity_value = state_value if entity_attr is None else state.attributes.get(entity_attr)
-            if entity_value == value:
-                _LOGGER.debug('%s(%s)=%s not changed', entity_id, entity_attr, entity_value)
+            to_value = entity_values[i]
+            current_value = state_value if entity_attr is None else state.attributes.get(entity_attr)
+            if current_value == to_value:
+                _LOGGER.debug('%s %s= %s not changed', entity_id, entity_attr or '', current_value)
                 return
 
-            data = {'entity_id': entity_id, service_attr or entity_attr: value}
-            _LOGGER.debug('%s(%s)=%s > %s\n  %s %s', entity_id, entity_attr, entity_value, value, service, data)
-            with AsyncTrackStates(_hass) as changed_states:
-                return await _hass.services.async_call(domain, service, data, True)
+            data = {'entity_id': entity_id, service_attr or entity_attr: to_value}
+            _LOGGER.info('%s %s= %s > %s, %s %s', entity_id, entity_attr or '', current_value, to_value, service, data)
+            _hass.services.call(domain, service or 'set_' + entity_attr, data, True)
             return
         else:
             i = i - 1
 
     if state_value == 'off':
-        _LOGGER.debug('%s(%s)=%s is already off', entity_id, entity_attr, entity_value)
+        _LOGGER.debug('%s %s is already off', entity_id, entity_attr or '')
         return
 
-    _LOGGER.debug('%s(%s)=%s > off', entity_id, entity_attr, entity_value)
-    with AsyncTrackStates(_hass) as changed_states:
-        return await _hass.services.async_call(domain, 'turn_off', {'entity_id': entity_id}, True)
+    _LOGGER.info('%s %s= %s > off', entity_id, entity_attr or '', state_value)
+    _hass.services.call(domain, 'turn_off', {'entity_id': entity_id}, True)
